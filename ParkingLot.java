53bec1993e1f8f5fc85f57adce3f97bf92e235d6
daminimali <148991044+daminimali@users.noreply.github.com>
2024-07-18 23:13:17 +0530

    Create ParkingLot.java

---
+++ ParkingLot.java
@@ -0,0 +1,29 @@
+// The function accepts a character array ‘arr’ of size ‘n’ as its argument. Each element of ‘arr’ represents the status of a parking slot, where ‘S’ represents an empty slot and ‘X’ represents an occupied slot. The function needs to return the maximum number of cars that can be parked in the parking lot. It is assumed that two cars cannot occupy the same slot and cars can only park in consecutive empty slots.
+import java.util.Scanner;
+public class ParkingLot {
+    public static int maxCarsParked(int n, char[] arr) {
+        int maxCars = 0;
+        int currentCars = 0;
+
+        for(int i=0;i<n;i++){
+            if(arr[i] == 'S'){
+                currentCars++;
+            }
+            else{
+                maxCars = Math.max(maxCars,currentCars);
+                currentCars = 0;
+            }
+        }
+        maxCars = Math.max(maxCars,currentCars);
+        return maxCars;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char arr[] = new char[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.next().charAt(0);
+        }
+        System.out.println(maxCarsParked(n,arr));
+    }
+}
