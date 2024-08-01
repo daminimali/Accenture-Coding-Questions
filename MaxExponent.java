4f956887c08a5c621f3a38b2d0da373bbcb3ca0a
daminimali <148991044+daminimali@users.noreply.github.com>
2024-08-01 20:10:49 +0530

    Create MaxExponent.java

---
+++ MaxExponent.java
@@ -0,0 +1,57 @@
+/*Problem Statement
+You are given a function,
+Int MaxExponents (int a , int b);
+You have to find and return the number between ‘a’ and ‘b’ ( range inclusive on both ends) which has the maximum exponent of 2.
+The algorithm to find the number with maximum exponent of 2 between the given range is
+Loop between ‘a’ and ‘b’. Let the looping variable be ‘i’.
+Find the exponent (power) of 2 for each ‘i’ and store the number with maximum exponent of 2 so faqrd in a variable , let say ‘max’. Set ‘max’ to ‘i’ only if ‘i’ has more exponent of 2 than ‘max’.
+Return ‘max’.
+Assumption: a <b
+Note: If two or more numbers in the range have the same exponents of  2 , return the small number.
+Example
+Input:
+7
+12
+Output:
+8
+Explanation:
+Exponents of 2 in:
+7-0
+8-3
+9-0
+10-1
+11-0
+12-2
+Hence maximum exponent if two is of 8.
+*/
+
+import java.util.Scanner;
+public class MaxExponent {
+    public static int maxExponent(int a, int b){
+        int maxNumber = a;
+        int maxExponent = 0;
+
+        for(int i=a;i<=b;i++){
+            int currentExponent = countExponent(i);
+            if(currentExponent > maxExponent){
+                maxExponent = currentExponent;
+                maxNumber = i;
+            }
+        }
+        return maxNumber;
+    }
+    public static int countExponent(int number){
+        int exponent = 0;
+        if(number % 2 == 0){
+            exponent++;
+            number /= 2;
+        }
+        return exponent;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(maxExponent(a,b));
+    }
+}
