256a50e0f429137066246614edec94d0fc6c6429
daminimali <148991044+daminimali@users.noreply.github.com>
2024-08-25 00:07:25 +0530

    Update ParkingLot.java

--- ParkingLot.java
+++ ParkingLot.java
@@ -20,10 +20,8 @@ public class ParkingLot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        char arr[] = new char[n];
-        for(int i=0;i<n;i++){
-            arr[i] = sc.next().charAt(0);
-        }
+        String str = sc.next();
+        char arr[] = str.toCharArray();
         System.out.println(maxCarsParked(n,arr));
     }
 }
